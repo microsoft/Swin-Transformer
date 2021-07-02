e39adac87887c03853148a5e3d87231c2de0b6f1
Ze Liu <liuzeeeee@gmail.com>
2021-07-03 00:19:17 +0800

    add swin mlp

--- models/build.py
+++ models/build.py
@@ -6,6 +6,7 @@
 # --------------------------------------------------------
 
 from .swin_transformer import SwinTransformer
+from .swin_mlp import SwinMLP
 
 
 def build_model(config):
@@ -27,6 +28,21 @@ def build_model(config):
                                 ape=config.MODEL.SWIN.APE,
                                 patch_norm=config.MODEL.SWIN.PATCH_NORM,
                                 use_checkpoint=config.TRAIN.USE_CHECKPOINT)
+    elif model_type == 'swin_mlp':
+        model = SwinMLP(img_size=config.DATA.IMG_SIZE,
+                        patch_size=config.MODEL.SWIN_MLP.PATCH_SIZE,
+                        in_chans=config.MODEL.SWIN_MLP.IN_CHANS,
+                        num_classes=config.MODEL.NUM_CLASSES,
+                        embed_dim=config.MODEL.SWIN_MLP.EMBED_DIM,
+                        depths=config.MODEL.SWIN_MLP.DEPTHS,
+                        num_heads=config.MODEL.SWIN_MLP.NUM_HEADS,
+                        window_size=config.MODEL.SWIN_MLP.WINDOW_SIZE,
+                        mlp_ratio=config.MODEL.SWIN_MLP.MLP_RATIO,
+                        drop_rate=config.MODEL.DROP_RATE,
+                        drop_path_rate=config.MODEL.DROP_PATH_RATE,
+                        ape=config.MODEL.SWIN_MLP.APE,
+                        patch_norm=config.MODEL.SWIN_MLP.PATCH_NORM,
+                        use_checkpoint=config.TRAIN.USE_CHECKPOINT)
     else:
         raise NotImplementedError(f"Unkown model: {model_type}")
 
