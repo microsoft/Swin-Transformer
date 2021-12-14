eed077f68e0386e8cdff2e1981492699d9c190c0
Ze Liu <liuze@mail.ustc.edu.cn>
2021-12-14 15:06:23 +0800

    Updating "_pil_interp" to keep compatible with torchvision>=0.10.0 (#152)

--- data/build.py
+++ data/build.py
@@ -13,10 +13,26 @@ from torchvision import datasets, transforms
 from timm.data.constants import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
 from timm.data import Mixup
 from timm.data import create_transform
-from timm.data.transforms import _pil_interp
 
 from .cached_image_folder import CachedImageFolder
 from .samplers import SubsetRandomSampler
+
+try:
+    from torchvision.transforms import InterpolationMode
+
+
+    def _pil_interp(method):
+        if method == 'bicubic':
+            return InterpolationMode.BICUBIC
+        elif method == 'lanczos':
+            return InterpolationMode.LANCZOS
+        elif method == 'hamming':
+            return InterpolationMode.HAMMING
+        else:
+            # default bilinear, do we want to allow nearest?
+            return InterpolationMode.BILINEAR
+except:
+    from timm.data.transforms import _pil_interp
 
 
 def build_loader(config):
