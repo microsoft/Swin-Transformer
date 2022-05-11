0ae00c97cb34ac03d40683c8319ccddd1b36ad8d
Ze Liu <liuze@mail.ustc.edu.cn>
2022-05-12 00:32:16 +0800

    add Swin Transformer V2 (#205)

--- models/build.py
+++ models/build.py
@@ -6,6 +6,7 @@
 # --------------------------------------------------------
 
 from .swin_transformer import SwinTransformer
+from .swin_transformer_v2 import SwinTransformerV2
 from .swin_mlp import SwinMLP
 
 
@@ -28,6 +29,23 @@ def build_model(config):
                                 ape=config.MODEL.SWIN.APE,
                                 patch_norm=config.MODEL.SWIN.PATCH_NORM,
                                 use_checkpoint=config.TRAIN.USE_CHECKPOINT)
+    elif model_type == 'swinv2':
+        model = SwinTransformerV2(img_size=config.DATA.IMG_SIZE,
+                                  patch_size=config.MODEL.SWINV2.PATCH_SIZE,
+                                  in_chans=config.MODEL.SWINV2.IN_CHANS,
+                                  num_classes=config.MODEL.NUM_CLASSES,
+                                  embed_dim=config.MODEL.SWINV2.EMBED_DIM,
+                                  depths=config.MODEL.SWINV2.DEPTHS,
+                                  num_heads=config.MODEL.SWINV2.NUM_HEADS,
+                                  window_size=config.MODEL.SWINV2.WINDOW_SIZE,
+                                  mlp_ratio=config.MODEL.SWINV2.MLP_RATIO,
+                                  qkv_bias=config.MODEL.SWINV2.QKV_BIAS,
+                                  drop_rate=config.MODEL.DROP_RATE,
+                                  drop_path_rate=config.MODEL.DROP_PATH_RATE,
+                                  ape=config.MODEL.SWINV2.APE,
+                                  patch_norm=config.MODEL.SWINV2.PATCH_NORM,
+                                  use_checkpoint=config.TRAIN.USE_CHECKPOINT,
+                                  pretrained_window_sizes=config.MODEL.SWINV2.PRETRAINED_WINDOW_SIZES)
     elif model_type == 'swin_mlp':
         model = SwinMLP(img_size=config.DATA.IMG_SIZE,
                         patch_size=config.MODEL.SWIN_MLP.PATCH_SIZE,
