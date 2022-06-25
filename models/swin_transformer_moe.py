8b0e38f56315dd21ba6603b7c65be35eb1e5bda0
Your Name <liuze@mail.ustc.edu.cn>
2022-06-25 23:19:39 +0800

    fix tutel import

--- models/swin_transformer_moe.py
+++ models/swin_transformer_moe.py
@@ -16,7 +16,7 @@ import numpy as np
 try:
     from tutel import moe as tutel_moe
 except:
-    tutel = None
+    tutel_moe = None
     print("Tutel has not been installed. To use Swin-MoE, please install Tutel; otherwise, just ignore this.")
 
 
