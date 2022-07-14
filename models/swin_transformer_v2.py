f0a0bbb123435cb14746fa783e22fd912233bb08
Hyunsoo Luke HA <dhy03196@naver.com>
2022-07-14 18:10:44 +0900

    Fix torch.clamp() issue #237

--- models/swin_transformer_v2.py
+++ models/swin_transformer_v2.py
@@ -153,7 +153,7 @@ class WindowAttention(nn.Module):
 
         # cosine attention
         attn = (F.normalize(q, dim=-1) @ F.normalize(k, dim=-1).transpose(-2, -1))
-        logit_scale = torch.clamp(self.logit_scale, max=torch.log(torch.tensor(1. / 0.01))).exp()
+        logit_scale = torch.clamp(self.logit_scale, max=torch.log(torch.tensor(1. / 0.01)).to(self.logit_scale.get_device())).exp()
         attn = attn * logit_scale
 
         relative_position_bias_table = self.cpb_mlp(self.relative_coords_table).view(-1, self.num_heads)
