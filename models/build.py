afeb877fba1139dfbc186276983af2abb02c2196
Li Tao <chntaoli@163.com>
2022-08-05 11:40:23 +0800

    Make use of apex fused kernels for training acceleration (#242)
    Make use of apex fused kernels for training acceleration 
    Co-authored-by: BestJuly <BestJuly@users.noreply.github.com>

--- models/build.py
+++ models/build.py
@@ -13,6 +13,19 @@ from .swin_mlp import SwinMLP
 
 def build_model(config):
     model_type = config.MODEL.TYPE
+
+    # accelerate layernorm
+    if config.FUSED_LAYERNORM:
+        try:
+            import apex as amp
+            layernorm = amp.normalization.FusedLayerNorm
+        except:
+            layernorm = None
+            print("To use FusedLayerNorm, please install apex.")
+    else:
+        import torch.nn as nn
+        layernorm = nn.LayerNorm
+
     if model_type == 'swin':
         model = SwinTransformer(img_size=config.DATA.IMG_SIZE,
                                 patch_size=config.MODEL.SWIN.PATCH_SIZE,
@@ -28,6 +41,7 @@ def build_model(config):
                                 drop_rate=config.MODEL.DROP_RATE,
                                 drop_path_rate=config.MODEL.DROP_PATH_RATE,
                                 ape=config.MODEL.SWIN.APE,
+                                norm_layer=layernorm,
                                 patch_norm=config.MODEL.SWIN.PATCH_NORM,
                                 use_checkpoint=config.TRAIN.USE_CHECKPOINT,
                                 fused_window_process=config.FUSED_WINDOW_PROCESS)
--- optimizer.py
+++ optimizer.py
@@ -7,6 +7,13 @@
 
 from torch import optim as optim
 
+try:
+    from apex.optimizers import FusedAdam, FusedLAMB
+except:
+    FusedAdam = None
+    FusedLAMB = None
+    print("To use FusedLAMB or FusedAdam, please install apex.")
+
 
 def build_optimizer(config, model):
     """
@@ -28,6 +35,12 @@ def build_optimizer(config, model):
     elif opt_lower == 'adamw':
         optimizer = optim.AdamW(parameters, eps=config.TRAIN.OPTIMIZER.EPS, betas=config.TRAIN.OPTIMIZER.BETAS,
                                 lr=config.TRAIN.BASE_LR, weight_decay=config.TRAIN.WEIGHT_DECAY)
+    elif opt_lower == 'fused_adam':
+        optimizer = FusedAdam(parameters, eps=config.TRAIN.OPTIMIZER.EPS, betas=config.TRAIN.OPTIMIZER.BETAS,
+                              lr=config.TRAIN.BASE_LR, weight_decay=config.TRAIN.WEIGHT_DECAY)
+    elif opt_lower == 'fused_lamb':
+        optimizer = FusedLAMB(parameters, eps=config.TRAIN.OPTIMIZER.EPS, betas=config.TRAIN.OPTIMIZER.BETAS,
+                              lr=config.TRAIN.BASE_LR, weight_decay=config.TRAIN.WEIGHT_DECAY)
 
     return optimizer
 
