9cd3dbd4a60ffc82f930f189284e46a5ad1f3bb0
dengmengling <dengmengling@meituan.com>
2022-08-29 19:51:08 +0800

    supported dynamic batch for torchscript trace

--- models/swin_transformer.py
+++ models/swin_transformer.py
@@ -68,9 +68,9 @@ def window_reverse(windows, window_size, H, W):
     Returns:
         x: (B, H, W, C)
     """
-    B = int(windows.shape[0] / (H * W / window_size / window_size))
-    x = windows.view(B, H // window_size, W // window_size, window_size, window_size, -1)
-    x = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(B, H, W, -1)
+    C = int(windows.shape[-1])
+    x = windows.view(-1, H // window_size, W // window_size, window_size, window_size, C)
+    x = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(-1, H, W, C)
     return x
 
 
