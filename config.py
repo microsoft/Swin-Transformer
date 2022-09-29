ca25717b4cea320f2d40676303db618867899e49
Tran Xuan Loc <loc.tran@moreh.com.vn>
2022-09-29 19:12:18 +0000

    reduce num of training epoch to 1 for testing

--- config.py
+++ config.py
@@ -146,7 +146,7 @@ _C.MODEL.SIMMIM.NORM_TARGET.PATCH_SIZE = 47
 # -----------------------------------------------------------------------------
 _C.TRAIN = CN()
 _C.TRAIN.START_EPOCH = 0
-_C.TRAIN.EPOCHS = 300
+_C.TRAIN.EPOCHS = 1
 _C.TRAIN.WARMUP_EPOCHS = 20
 _C.TRAIN.WEIGHT_DECAY = 0.05
 _C.TRAIN.BASE_LR = 5e-4
