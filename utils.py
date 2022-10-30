86e21c92fffb104b034641a22219752c32c13fcd
Sam Stevens <samuel.robert.stevens@gmail.com>
2022-10-30 18:33:55 +0000

    Make it possible to start from hierarchical checkpoints

--- utils.py
+++ utils.py
@@ -49,6 +49,156 @@ def load_checkpoint(config, model, optimizer, lr_scheduler, loss_scaler, logger)
     return max_accuracy
 
 
+def _hierarchical_weight_k(i):
+    return f"head.heads.{i}.weight"
+
+
+def _hierarchical_bias_k(i):
+    return f"head.heads.{i}.bias"
+
+
+def handle_linear_head(config, model, state_dict, logger):
+    """
+    Check classifier, if not match, then re-init classifier to zero
+    Ways it could not match:
+
+    1. Both have a single linear head, with different number of classes
+
+    2. Pretrained with hierarchical head and are now finetuning with single linear head.
+       If the fine-grained head was 10K classes and the linear head has 10K classes,
+       and the current dataset is iNat, we use the pre-trained fine-grained head.
+
+    3. Pretrained with single linear head and are finetuning with hierarchical head
+       (for instance, if starting with imagenet pre-training, then doing domain-specific
+       pre-training on iNat21)
+
+    4. Both have a hierarchical head with different number of tiers/classes.
+       We always reinitialize the hierarchical head.
+    """
+
+    pretrained_hierarchical = _hierarchical_bias_k(0) in state_dict
+    current_hierarchical = config.HIERARCHICAL
+
+    if not pretrained_hierarchical and not current_hierarchical:
+        # TESTED because Microsoft wrote this code.
+        # Both have a single linear head
+        assert "head.bias" in state_dict, "Should have a single pre-trained linear head"
+        assert hasattr(model.head, "bias"), "Should have a single random linear head"
+
+        head_bias_pretrained = state_dict["head.bias"]
+        num_classes_pretrained = head_bias_pretrained.shape[0]
+        num_classes = model.head.bias.shape[0]
+        if num_classes_pretrained != num_classes:
+            if num_classes_pretrained == 21841 and num_classes == 1000:
+                logger.info("loading ImageNet-22K weight to ImageNet-1K ......")
+                map22kto1k_path = "data/map22kto1k.txt"
+                with open(map22kto1k_path) as f:
+                    map22kto1k = f.readlines()
+                map22kto1k = [int(id22k.strip()) for id22k in map22kto1k]
+                state_dict["head.weight"] = state_dict["head.weight"][map22kto1k, :]
+                state_dict["head.bias"] = state_dict["head.bias"][map22kto1k]
+            else:
+                torch.nn.init.constant_(model.head.bias, 0.0)
+                torch.nn.init.constant_(model.head.weight, 0.0)
+                del state_dict["head.weight"]
+                del state_dict["head.bias"]
+                logger.warning(
+                    "Error in loading classifier head, re-init classifier head to 0"
+                )
+    elif pretrained_hierarchical and not current_hierarchical:
+        # UNTESTED
+        assert (
+            "head.bias" not in state_dict
+        ), "Should not have a single pre-trained linear head"
+        assert hasattr(model.head, "bias"), "Should have a single random linear head"
+        # Increment finegrained level until the key doesn't exist.
+        # Then it is the last level in the hierarchical model
+        max_level = 0
+        while _hierarchical_bias_k(max_level) in state_dict:
+            max_level += 1
+
+        finegrained_num_classes_pretrained = state_dict[
+            _hierarchical_bias_k(max_level)
+        ].shape[0]
+        num_classes = model.head.bias.shape[0]
+        if num_classes == finegrained_num_classes_pretrained == 10_000:
+            # Probably fine-tuning on iNat21
+            logger.warn(
+                "Assuming that you pre-trained on iNat21 and are now fine-tuning on iNat21."
+            )
+            state_dict["head.weight"] = state_dict[_hierarchical_weight_k(max_level)]
+            state_dict["head.bias"] = state_dict[_hierarchical_bias_k(max_level)]
+        else:
+            for i in range(max_level):
+                del state_dict[_hierarchical_weight_k(i)]
+                del state_dict[_hierarchical_bias_k(i)]
+            logger.warning(
+                "Error in loading classifier head, using default initialization."
+            )
+    elif not pretrained_hierarchical and current_hierarchical:
+        # UNTESTED
+        assert "head.bias" in state_dict, "Should have a single pre-trained linear head"
+        assert not hasattr(
+            model.head, "bias"
+        ), "Should not have a single random linear head"
+
+        # Delete the head.bias and head.weight keys then do nothing since the linear
+        # layer is already correctly initialized from scratch so it can fine-tune.
+        del state_dict["head.weight"]
+        del state_dict["head.bias"]
+
+    elif pretrained_hierarchical and current_hierarchical:
+        assert (
+            "head.bias" not in state_dict
+        ), "Should not have a single pre-trained linear head"
+        assert not hasattr(
+            model.head, "bias"
+        ), "Should not have a single random linear head"
+
+        # Check if the two models have the exact same number of levels, and the same
+        # number of classes in each level
+        matches = True
+        level = 0
+        while matches and _hierarchical_bias_k(level) in state_dict:
+            # Check that the current model has the right attribute
+            if level > len(model.head.heads):
+                matches = False
+                continue
+
+            if not hasattr(model.head.heads[level], "bias"):
+                matches = False
+                continue
+
+            if (
+                model.head.heads[level].bias.shape
+                != state_dict[_hierarchical_bias_k(level)].shape
+            ):
+                matches = False
+                continue
+
+            if (
+                model.head.heads[level].weight.shape
+                != state_dict[_hierarchical_weight_k(level)].shape
+            ):
+                matches = False
+                continue
+
+            level += 1
+
+        if not matches:
+            # UNTESTED
+            logger.warning(
+                "Not using pre-trained hierarchical head because the shapes do not match."
+            )
+            # Delete the keys from the state dict because the pre-trained model and
+            # the current model do not match in size.
+            for i in range(level):
+                del state_dict[_hierarchical_weight_k(i)]
+                del state_dict[_hierarchical_bias_k(i)]
+        else:
+            logger.info("Using pre-trained hierarchical head.")
+
+
 def load_pretrained(config, model, logger):
     logger.info(
         f"==============> Loading weight {config.MODEL.PRETRAINED} for fine-tuning......"
@@ -137,29 +287,15 @@ def load_pretrained(config, model, logger):
                 )
                 state_dict[k] = absolute_pos_embed_pretrained_resized
 
-    # check classifier, if not match, then re-init classifier to zero
-    head_bias_pretrained = state_dict["head.bias"]
-    Nc1 = head_bias_pretrained.shape[0]
-    Nc2 = model.head.bias.shape[0]
-    if Nc1 != Nc2:
-        if Nc1 == 21841 and Nc2 == 1000:
-            logger.info("loading ImageNet-22K weight to ImageNet-1K ......")
-            map22kto1k_path = f"data/map22kto1k.txt"
-            with open(map22kto1k_path) as f:
-                map22kto1k = f.readlines()
-            map22kto1k = [int(id22k.strip()) for id22k in map22kto1k]
-            state_dict["head.weight"] = state_dict["head.weight"][map22kto1k, :]
-            state_dict["head.bias"] = state_dict["head.bias"][map22kto1k]
-        else:
-            torch.nn.init.constant_(model.head.bias, 0.0)
-            torch.nn.init.constant_(model.head.weight, 0.0)
-            del state_dict["head.weight"]
-            del state_dict["head.bias"]
-            logger.warning(
-                f"Error in loading classifier head, re-init classifier head to 0"
-            )
+    handle_linear_head(config, model, state_dict, logger)
 
     msg = model.load_state_dict(state_dict, strict=False)
+    for key in msg.missing_keys:
+        assert (
+            "relative_coords_table" in key
+            or "relative_position_index" in key
+            or "attn_mask" in key
+        ), f"Should only reinitialize relative positional information, not '{key}'"
     logger.warning(msg)
 
     logger.info(f"=> loaded successfully '{config.MODEL.PRETRAINED}'")
