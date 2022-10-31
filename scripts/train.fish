0c418901603d29304a94c08c41f51b2b2250f30d
Samuel Stevens <samuel.robert.stevens@gmail.com>
2022-10-31 14:36:29 -0400

    Add default config

--- scripts/train.fish
+++ scripts/train.fish
@@ -8,6 +8,7 @@ function usage
     echo "  -h/--help  print this message"
     echo "  --config   which config file to use (should be YAML)"
     echo "  --debug    run with only one process rather than 8 so pdb is useable."
+    echo "  --nprocs   number of processes"
     echo "  --tag      tag for the run (I use v0, v1, etc)"
     echo "  --venv     path to the virtual environment (default ./venv/)"
 end
@@ -15,7 +16,8 @@ end
 set -l options (fish_opt --short h --long help)
 set -a options (fish_opt --short c --long config --required-val --long-only)
 set -a options (fish_opt --short t --long tag --required-val --long-only)
-set -a options (fish_opt --short v --long venv --long-only)
+set -a options (fish_opt --short p --long nprocs --required-val --long-only)
+set -a options (fish_opt --short v --long venv --required-val --long-only)
 set -a options (fish_opt --short d --long debug --long-only)
 
 argparse $options -- $argv
@@ -40,14 +42,20 @@ if not test -z $_flag_venv
     set venv $_flag_venv
 end
 
+set nprocs 8
+if not test -z $_flag_nprocs
+    set nprocs $_flag_nprocs
+end
+
 set launcher $venv/bin/torchrun
-set launcher_args --nproc_per_node 8 --master_port 12345
+set launcher_args --nproc_per_node $nprocs
 
 if not test -z $_flag_debug
-    set launcher_args --nproc_per_node 1 --master_port 12345
+    set launcher_args --nproc_per_node 1
 end
 
 $launcher $launcher_args \
+    --master_port 12345 \
     main.py \
     --cfg $_flag_config \
     --output runs \
